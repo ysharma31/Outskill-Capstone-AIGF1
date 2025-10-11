dbcee4602ea1271c3ebdddb783204f499bafabc3
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2025-10-10 18:16:00 -0700

    Updated supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = ''
-const supabaseAnonKey = ''
+const supabaseUrl = 'https://smhmuegdoucznluneftm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtaG11ZWdkb3Vjem5sdW5lZnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MjMxNTYsImV4cCI6MjA3NTQ5OTE1Nn0.dBcCg_esHz5UbHyAaccYUUlZevcykXzL6Cnb-2PltZ8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
